8503c108babd8966ee9ed472c59b6e98383b6041
pedrofonseca1227 <152933807+pedrofonseca1227@users.noreply.github.com>
2024-06-10 14:17:46 -0300

    Add files via upload

---
+++ ChatGPT-APIS-master/src/components/Chat.vue
@@ -0,0 +1,91 @@
+<template>
+    <v-sheet
+        class="mx-auto mt-1 container"
+        elevation="4"
+        max-width="85%"
+    >
+    <Message v-for="(message, index) in messages" :key="index" :msg="message" />
+    </v-sheet>
+    <v-sheet
+        class="mx-auto mt-1 container pa-5"
+        elevation="4"
+        max-width="85%"
+        height="auto"
+    >
+        <div class="d-flex">
+            <v-textarea
+                label="prompt"
+                variant="underlined"
+                class="me-5"
+                v-model="message"
+            ></v-textarea>
+            <v-btn
+                @click="userInput"
+                icon="mdi-send"
+                class="align-self-center"
+            ></v-btn>
+           
+        </div>
+    </v-sheet>
+</template>
+
+<script>
+import Message from './Message.vue'
+import {reactive, ref} from 'vue'
+import openai from '../helpers/useOpenAi'
+
+export default {
+    components: {
+        Message
+    },
+    setup() {
+        const message = ref(null)
+        const messages =  reactive([])
+
+        const userInput = () => {
+            messages.push({
+                role: 'user',
+                content: message.value
+            })
+            message.value = null
+            chat(messages)
+        }
+
+        const chat = async (msgs) => {
+            const chatCompletion = await openai.chat.completions.create({
+                messages: msgs,
+                model: 'gpt-3.5-turbo',
+                stream: true
+            })
+
+            for await (const chunk of chatCompletion) {
+
+
+                if(chunk.choices) {
+                    if(messages[messages.length - 1].role === 'assistant') {
+                        messages[messages.length -1].content += chunk.choices[0].delta.content
+                    } else {
+                        messages.push({
+                            role: 'assistant',
+                            content: chunk.choices[0].delta.content
+                        })
+                    }
+
+                }
+
+            }
+
+        }
+
+        return {userInput, message, messages}
+    },
+}
+</script>
+
+<style scoped>
+    .container {
+        max-height: 75hv;
+        overflow-y: auto;
+        flex-direction: column;
+    }
+</style>
